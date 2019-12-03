d5e8fae1a808ec0561290d07a4d5df6badb22599
Benjamin Schwessinger <benjamin.schwessinger@anu.edu.au>
2019-12-03 16:38:05 +1100

    I added the second gene2 for expression.

--- test_script.py
+++ test_script.py
@@ -17,12 +17,16 @@ data = pd.read_excel('12864_2016_2684_MOESM1_ESM.xlsx', sheet_name = 'Table S20'
 #-----------------------
 # Get the gene name from the command line
 gene = sys.argv[1]
+gene2 = sys.argv[2]
 
 try:
         data.loc[gene]
 except:
         print("Please provide a gene name.")
         sys.exit()
+
+
+
 
 # Print the gene expression
 print('Expression at 0 dpi: ', data.loc[gene]['0dpi'])
@@ -33,3 +37,8 @@ print('Expression at 5 dpi: ', data.loc[gene]['5dpi'])
 print('Expression at 7 dpi: ', data.loc[gene]['7dpi'])
 print('Expression at 9 dpi: ', data.loc[gene]['9dpi'])
 print('Expression at 11 dpi: ', data.loc[gene]['11dpi'])
+
+
+#Print expression second gene
+
+print('Expression at 0 dpi: ', data.loc[gene2]['0dpi'])
